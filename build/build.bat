089762f2aeee483e06543fa81882dd4ab2dcc881
RogerNogue <rogernogueb@gmail.com>
2019-10-06 20:10:11 +0200

    Added a new necessary lib for the painting process

--- build/build.bat
+++ build/build.bat
@@ -4,6 +4,6 @@
 mkdir C:\Users\Roger\Desktop\Roger\handmade\buildStuff
 REM popd removes pushd makings
 pushd C:\Users\Roger\Desktop\Roger\handmade\buildStuff
-cl /Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib
+cl /Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib gdi32.lib
 REM cl /Zi ..\code\win32_handmade.cpp
 popd
