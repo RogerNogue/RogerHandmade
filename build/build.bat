140ef251a24a31064f2f4b53c31f8c4d11a68c67
RogerNogue <rogernogueb@gmail.com>
2019-10-17 13:41:20 +0200

    added some remarks that could be interesting

--- build/build.bat
+++ build/build.bat
@@ -4,6 +4,8 @@
 mkdir C:\Users\Roger\Desktop\Roger\handmade\buildStuff
 REM popd removes pushd makings
 pushd C:\Users\Roger\Desktop\Roger\handmade\buildStuff
+REM "-Wall" flag to print all warnings
 cl /Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib gdi32.lib
 REM cl /Zi ..\code\win32_handmade.cpp
+REM -FC to show full paths
 popd
