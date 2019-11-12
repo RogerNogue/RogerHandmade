34adca9695c912e9be142123757ae4ca94c523bc
RogerNogue <rogernogueb@gmail.com>
2019-11-12 15:27:50 +0100

    added a comment to the build script that explains how to add stack memory to the programm

--- build/build.bat
+++ build/build.bat
@@ -5,7 +5,8 @@ mkdir C:\Users\Roger\Desktop\Roger\handmade\buildStuff
 REM popd removes pushd makings
 pushd C:\Users\Roger\Desktop\Roger\handmade\buildStuff
 REM "-Wall" flag to print all warnings
-cl /Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib gdi32.lib
+cl -Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib gdi32.lib
 REM cl /Zi ..\code\win32_handmade.cpp
 REM -FC to show full paths
+REM cl -F4194304 ... requests for a 4MB stack size
 popd
