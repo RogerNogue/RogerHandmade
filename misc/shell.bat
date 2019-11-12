34d71948055e4af324de0495f2eccec9e4529168
RogerNogue <rogernogueb@gmail.com>
2019-11-12 15:27:01 +0100

    Updated my visual studio routes, so I had to change the script that sets up the compilation environment

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,3 +1,4 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 REM it was the old route
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set path=C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\misc;C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE;%path%;
