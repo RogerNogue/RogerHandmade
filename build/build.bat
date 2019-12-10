e132ccfb150c4c3eb7fcca8dbfbd90ce07df781d
RogerNogue <rogernogueb@gmail.com>
2019-12-10 17:25:21 +0100

    Added a build flag that indicates running on windows

--- build/build.bat
+++ build/build.bat
@@ -5,7 +5,8 @@ mkdir C:\Users\Roger\Desktop\Roger\handmade\buildStuff
 REM popd removes pushd makings
 pushd C:\Users\Roger\Desktop\Roger\handmade\buildStuff
 REM "-Wall" flag to print all warnings
-cl -Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib gdi32.lib
+REM -O2 for optimization
+cl -DHANDMADE_WIN32=1 -Zi C:\Users\Roger\Desktop\Roger\handmade\RogerHandmade\code\win32_handmade.cpp user32.lib gdi32.lib
 REM cl /Zi ..\code\win32_handmade.cpp
 REM -FC to show full paths
 REM cl -F4194304 ... requests for a 4MB stack size
